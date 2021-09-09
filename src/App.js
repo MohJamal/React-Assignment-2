9a5d8c04e50260c429fe7e747d7db318c4ab693e
MohJamal <mohjamalalkhatib96@gmail.com>
2021-09-09 03:33:42 +0300

    task 2 is done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Comp1 from "./Components/Comp1";
+import Comp2 from "./Components/Comp2";
 
 function App() {
   return (
     <div className="App">
       <Comp1 />
+      <Comp2 />
     </div>
   );
 }
--- src/Components/Comp2.js
+++ src/Components/Comp2.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Comp2 = () => {
-  return <div></div>;
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+
+  const usernameChangeHandler = (event) => {
+    let enteredUsername = event.target.value;
+    enteredUsername.length !== 0
+      ? setUsername(`Username: ${enteredUsername}`)
+      : setUsername("");
+  };
+
+  const emailChangeHandler = (event) => {
+    let enteredEmail = event.target.value;
+    enteredEmail.length !== 0
+      ? setEmail(`Email: ${enteredEmail}`)
+      : setEmail("");
+  };
+
+  return (
+    <div>
+      <p>
+        {username}
+        <br />
+        {email}
+      </p>
+      <form>
+        <input
+          type="text"
+          onChange={usernameChangeHandler}
+          placeholder="Username"
+        />
+        <input type="email" onChange={emailChangeHandler} placeholder="Email" />
+      </form>
+    </div>
+  );
 };
 
 export default Comp2;
